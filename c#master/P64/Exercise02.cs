4a138d19c71d52e03968c74b6dbed18fb253ea55
Otaki Takaya <t.otaki@earth.sint.co.jp>
2022-06-02 17:23:14 +0900

    今日の課題2

---
+++ c#master/P64/Exercise02.cs
@@ -0,0 +1,15 @@
+﻿/*
+課題2
+接続型を使用して、IntDataとStringDataを入力させて、IntDataに該当するレコードのStringDataを更新しなさい。
+*/
+
+namespace CSMasterADONET.Exercise
+{
+    class Exercise02
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
