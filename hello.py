cdb929b4094cfe809a9ef7eed1de0da0d9a76a4c
linelime <linelime@naver.com>
2022-07-10 22:46:21 +0900

    test

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
-def hello():
-    print("hello world")
-    
-hello()
+array = {1,999,3, 422,31, 50, 99, 220, 300}
+
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(len(array)-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+
+bubble_sort(array)
+
+print (array)
